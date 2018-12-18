67c012b4792152e1244b27b92d19d77dcaf2e7c5
Tommy Yan <tylyan@gmail.com>
2018-12-17 22:32:34 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# iloveyousomuch
+
+Just a little something for the wifey
+
+[hosted here](http://iloveyousomuch.s3-website-us-east-1.amazonaws.com/)
---
+++ js/index.js
@@ -0,0 +1,42 @@
+const anniversary = moment("12-14-2017 14:00 +0000", "MM-DD-YYYY HH:mm Z")
+const today = moment("+0000", "Z")
+const totalDays = today.diff(anniversary, 'days')
+const years = Math.floor(totalDays / 365)
+const days = totalDays % 365
+let toggle = true
+
+toggleDays = () => {
+	if (toggle) {
+		daysOnly(totalDays)
+	} else {
+		yearsAndDays(years, days)
+	}
+	toggle = !toggle
+}
+
+yearsAndDays = (years, days) => {
+	let msg = ''
+	if (years === 0) {
+		daysOnly(days)
+		return
+	}
+	document.getElementById('counter').innerHTML = 'happy ' + years + ' year ' + days + ' day!'
+}
+
+daysOnly = (days) => {
+	document.getElementById('counter').innerHTML = 'happy ' + days + ' day!'
+}
+
+isAnniversary = () => {
+	return days === 0
+}
+
+window.onload = () => {
+	if (isAnniversary()) {
+		const toggle = document.getElementById('toggleButton')
+		toggle.parentNode.removeChild(toggle)
+		document.getElementById('counter').innerHTML = 'happy ' + years + ' anniversary!~'
+	} else {
+		yearsAndDays(years, days)
+	}
+}
